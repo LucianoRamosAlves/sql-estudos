6c643accdbf7a21ce07a7bdca8e9b7c99211f862
Luciano Ramos <lramosalves37@gmail.com>
2026-05-07 08:23:57 -0300

    Add notes on running MySQL scripts
    Create advanced/command.sql with concise CLI notes for executing MySQL scripts. Covers using mysql client and mysqlsh, connecting with \connect/root@localhost, using -u root -p, mysql_config_editor to store credentials, and an example of redirecting a .sql file from the shell.

---
+++ advanced/command.sql
@@ -0,0 +1,16 @@
+-- posso usar o cmd para executar um script no mysql
+-- mysql -u root < script.sql
+
+-- começa com mysqlsh 
+-- mysqlsh  -- depois \sql para executar o script
+
+-- \connect root@localhost para logar
+--- senha
+
+-- no cmd raiz uso -u root -p
+
+-- mysql_config_editor set --host=localhost --user=root --password com iiso eu salvo a senha
+
+-- depois mysql -u root, entra sem senha
+
+-- ysql -u root -D word -p < "C:\Users\lramo\OneDrive\Documentos\Estudos\sql-estudos\objects\function\function.sql" aqui eu entro usando o arquivo
