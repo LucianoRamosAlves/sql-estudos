ea1d940fad6040da71ffa7db9692ae20c17b47c3
Luciano Ramos <lramosalves37@gmail.com>
2026-05-13 16:47:47 -0300

    update \tables.sql

---
+++ project/election_etl_project/sql/schema/tables.sql
@@ -0,0 +1,36 @@
+USE election_etl_project;
+
+-- -----------------------------
+-- -------- PARTE BRONZE -------
+-- -----------------------------
+
+
+CREATE TABLE IF NOT EXISTS bronze_votos(
+    titulo_eleitor INT,
+    candidato_id INT,
+    eleitor VARCHAR(100),
+    cidade VARCHAR(100),
+    data_voto DATE
+);
+
+
+CREATE TABLE IF NOT EXISTS bronze_candidatos(
+    candidato_id INT,
+    nome VARCHAR(100),
+    partido VARCHAR(100),
+    cargo VARCHAR(100),
+    cidade VARCHAR(100),
+    idade INT
+);
+
+
+CREATE TABLE IF NOT EXISTS bronze_cargos(
+    cargo_id INT,
+    nome_cargo VARCHAR(100),
+    esfera VARCHAR(100),
+    vagas INT,
+    ano_eleicao DATE
+);
+
+-- confirmar tabelas criadas
+SHOW TABLES;
