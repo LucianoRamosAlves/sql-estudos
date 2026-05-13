65675af593f98dbd35d421fab3b205fe4fb40cf4
Luciano Ramos <lramosalves37@gmail.com>
2026-05-13 20:24:43 -0300

    create load_cargos.sql

---
+++ project/election/sql/imports/load_cargos.sql
@@ -0,0 +1,48 @@
+-- seleciona o banco de dados que será utilizado
+USE election;
+
+-- remove todos os dados antigos da tabela staging
+-- staging table = tabela temporária de carga
+TRUNCATE TABLE bronze_cargos;
+
+-- LOCAL = arquivo localizado na máquina cliente
+LOAD DATA LOCAL INFILE
+'/mnt/c/Users/lramo/OneDrive/Documentos/Estudos/sql-estudos/project/election/data/raw/cargos/raw_cargos.csv'
+
+
+
+-- tabela que receberá os dados
+INTO TABLE bronze_cargos
+
+--    -------------------------------------------------------------------
+    -- CONFIGURAÇÃO DO CSV
+  --  -------------------------------------------------------------------
+
+    -- colunas separadas por vírgula
+    FIELDS TERMINATED BY ','
+
+    -- textos envolvidos por aspas
+    ENCLOSED BY '"'
+
+    -- quebra de linha do Windows
+    LINES TERMINATED BY '\r\n'
+
+--    -------------------------------------------------------------------
+    -- IGNORA CABEÇALHO
+ --   -------------------------------------------------------------------
+
+    -- ignora a primeira linha do arquivo csv
+    IGNORE 1 ROWS
+
+ --   -------------------------------------------------------------------
+    -- MAPEAMENTO DAS COLUNAS
+ --   -------------------------------------------------------------------
+
+-- ordem das colunas do csv
+(
+cargo_id,
+nome_cargo,
+esfera,
+vagas,
+ano_eleicao
+)  
