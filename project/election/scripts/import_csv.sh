2eb3541c893809c8265f2c784e919b94317d391a
Luciano Ramos <lramosalves37@gmail.com>
2026-05-14 14:12:11 -0300

    update \import_csv.sh

--- project/election/scripts/import_csv.sh
+++ project/election/scripts/import_csv.sh
@@ -30,6 +30,9 @@ source config/.env
 # pasta raiz do projeto
 PROJECT_PATH="/mnt/c/Users/lramo/OneDrive/Documentos/Estudos/sql-estudos/project/election"
 
+# pasta onde ficam os arquivos processados
+ARCHIVE_PATH="$PROJECT_PATH/data/history/imports"
+
 # pasta onde ficam os arquivos sql de importação
 SQL_PATH="$PROJECT_PATH/sql/imports"
 
@@ -50,6 +53,12 @@ DATE=$(date '+%Y-%m-%d %H:%M:%S')
 # o -p evita erro se a pasta já existir
 
 mkdir -p "$LOG_PATH"
+
+mkdir -p "$ARCHIVE_PATH/h_cargos"
+mkdir -p "$ARCHIVE_PATH/h_votos"
+mkdir -p "$ARCHIVE_PATH/h_candidatos"
+
+
 
 # =====================================================
 # DEFINE NOME DO LOG
@@ -104,16 +113,18 @@ echo "[$DATE] START IMPORT bronze_cargos" >> "$LOG_FILE"
 # com 2>&1:
 # tudo vai para o mesmo arquivo
 
-mysql --local-infile=1 \
--h $DB_HOST \
--D $DB_NAME \
--u $DB_USER \
--p$DB_PASSWORD \
-< "$SQL_PATH/load_cargos.sql" \
+# =====================================================
+# TESTA CONEXÃO MYSQL
+# =====================================================
+
+echo "[$DATE] Testando conexão com MySQL..." >> "$LOG_FILE"
+
+mysql -sN \
+-e "SELECT 1;" \
 >> "$LOG_FILE" 2>&1
 
 # =====================================================
-# VERIFICA SE O MYSQL EXECUTOU COM SUCESSO
+# VERIFICA SE O MYSQL EXECUTOU COM SUCESSO e CONEXAÇO
 # =====================================================
 
 # $? = status do último comando executado
@@ -121,23 +132,68 @@ mysql --local-infile=1 \
 # 0 = sucesso
 # diferente de 0 = erro
 
+# =====================================================
+# VERIFICA CONEXÃO
+# =====================================================
+
 if [ $? -eq 0 ]; then
 
-    # grava sucesso no log
+    echo "[$DATE] Conexão MySQL estabelecida com sucesso" >> "$LOG_FILE"
 
-    echo "[$DATE] SUCCESS bronze_cargos" >> "$LOG_FILE"
+    # =====================================================
+    # IMPORTAÇÃO CSV
+    # =====================================================
+
+    echo "[$DATE] Carregando CSV cargos..." >> "$LOG_FILE"
+
+    mysql --local-infile=1 \
+    < "$SQL_PATH/load_cargos.sql" \
+    >> "$LOG_FILE" 2>&1
+
+    # =====================================================
+    # VERIFICA IMPORTAÇÃO
+    # =====================================================
+
+    if [ $? -eq 0 ]; then
+
+        echo "[$DATE] CSV carregado com sucesso" >> "$LOG_FILE"
+
+        echo "[$DATE] Dados inseridos com sucesso" >> "$LOG_FILE"
+
+        mv "$PROJECT_PATH/data/raw/cargos/raw_cargos.csv" \
+        "$ARCHIVE_PATH/h_cargos/h_cargos_$(date '+%Y%m%d_%H%M%S').csv"
+
+        echo "[$DATE] PROCESSO EXECUTADO COM SUCESSO" >> "$LOG_FILE"
+
+        echo "=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=" >> "$LOG_FILE"
+
+    else
+
+        echo "[$DATE] ERRO ao importar CSV cargos" >> "$LOG_FILE"
+
+        echo "=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=" >> "$LOG_FILE"
+
+    fi
 
 else
 
-    # grava erro no log
+    echo "[$DATE] ERRO na conexão com MySQL" >> "$LOG_FILE"
 
-    echo "[$DATE] ERROR bronze_cargos" >> "$LOG_FILE"
+    echo "=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=" >> "$LOG_FILE"
 
 fi
+
+# =====================================================
+# GERA HASH DE INTEGRIDADE
+# =====================================================
+
+sha256sum "$LOG_FILE" > "$LOG_FILE.sha256"
 
 
 # =====================================================
 # FIM DO PROCESSO
+# =====================================================
+
 
 
 
@@ -153,29 +209,59 @@ LOG_FILE="$LOG_PATH/import_votos.log"
 
 echo "[$DATE] START IMPORT bronze_votos" >> "$LOG_FILE"
 
+echo "[$DATE] Testando conexão com MySQL..." >> "$LOG_FILE"
 
-mysql --local-infile=1 \
--h $DB_HOST \
--D $DB_NAME \
--u $DB_USER \
--p$DB_PASSWORD \
-< "$SQL_PATH/load_votos.sql" \
+mysql -sN \
+-e "SELECT 1;" \
 >> "$LOG_FILE" 2>&1
 
 
 if [ $? -eq 0 ]; then
 
-    # grava sucesso no log
+    echo "[$DATE] Conexão MySQL estabelecida com sucesso" >> "$LOG_FILE"
 
-    echo "[$DATE] SUCCESS bronze_votos" >> "$LOG_FILE"
+    echo "[$DATE] Carregando CSV votos..." >> "$LOG_FILE"
+
+    mysql --local-infile=1 \
+    < "$SQL_PATH/load_votos.sql" \
+    >> "$LOG_FILE" 2>&1
+
+    if [ $? -eq 0 ]; then
+
+        echo "[$DATE] CSV carregado com sucesso" >> "$LOG_FILE"
+
+        echo "[$DATE] Dados inseridos com sucesso" >> "$LOG_FILE"
+
+        mv "$PROJECT_PATH/data/raw/votos/raw_votos.csv" \
+        "$ARCHIVE_PATH/h_votos/h_votos_$(date '+%Y%m%d_%H%M%S').csv"
+
+        echo "[$DATE] PROCESSO EXECUTADO COM SUCESSO" >> "$LOG_FILE"
+
+
+
+        echo "=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=" >> "$LOG_FILE"
+
+    else
+
+        echo "[$DATE] ERRO ao importar CSV votos" >> "$LOG_FILE"
+
+        echo "=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=" >> "$LOG_FILE"
+
+    fi
 
 else
 
-    # grava erro no log
+    echo "[$DATE] ERRO na conexão com MySQL" >> "$LOG_FILE"
 
-    echo "[$DATE] ERROR bronze_votos" >> "$LOG_FILE"
+    echo "=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=" >> "$LOG_FILE"
 
 fi
+
+# =====================================================
+# GERA HASH DE INTEGRIDADE
+# =====================================================
+
+sha256sum "$LOG_FILE" > "$LOG_FILE.sha256"
 
 
 # =====================================================
@@ -196,28 +282,57 @@ LOG_FILE="$LOG_PATH/import_candidatos.log"
 echo "[$DATE] START IMPORT bronze_candidatos" >> "$LOG_FILE"
 
 
-mysql --local-infile=1 \
--h $DB_HOST \
--D $DB_NAME \
--u $DB_USER \
--p$DB_PASSWORD \
-< "$SQL_PATH/load_candidatos.sql" \
+echo "[$DATE] Testando conexão com MySQL..." >> "$LOG_FILE"
+
+mysql -sN \
+-e "SELECT 1;" \
 >> "$LOG_FILE" 2>&1
 
 
 if [ $? -eq 0 ]; then
 
-    # grava sucesso no log
+    echo "[$DATE] Conexão MySQL estabelecida com sucesso" >> "$LOG_FILE"
 
-    echo "[$DATE] SUCCESS bronze_candidatos" >> "$LOG_FILE"
+    echo "[$DATE] Carregando CSV candidatos..." >> "$LOG_FILE"
+
+    mysql --local-infile=1 \
+    < "$SQL_PATH/load_candidatos.sql" \
+    >> "$LOG_FILE" 2>&1
+
+    if [ $? -eq 0 ]; then
+
+        echo "[$DATE] CSV carregado com sucesso" >> "$LOG_FILE"
+
+        echo "[$DATE] Dados inseridos com sucesso" >> "$LOG_FILE"
+
+        echo "[$DATE] PROCESSO EXECUTADO COM SUCESSO" >> "$LOG_FILE"
+
+        mv "$PROJECT_PATH/data/raw/candidatos/raw_candidatos.csv" \
+        "$ARCHIVE_PATH/h_candidatos/h_candidatos_$(date '+%Y%m%d_%H%M%S').csv"
+
+        echo "=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=" >> "$LOG_FILE"
+
+    else
+
+        echo "[$DATE] ERRO ao importar CSV candidatos" >> "$LOG_FILE"
+
+        echo "=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=" >> "$LOG_FILE"
+
+    fi
 
 else
 
-    # grava erro no log
+    echo "[$DATE] ERRO na conexão com MySQL" >> "$LOG_FILE"
 
-    echo "[$DATE] ERROR bronze_candidatos" >> "$LOG_FILE"
+    echo "=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=" >> "$LOG_FILE"
 
 fi
+
+# =====================================================
+# GERA HASH DE INTEGRIDADE
+# =====================================================
+
+sha256sum "$LOG_FILE" > "$LOG_FILE.sha256"
 
 
 # =====================================================
