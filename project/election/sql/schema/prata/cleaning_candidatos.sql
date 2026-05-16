4bd23db23f8ca3766508e68d4a4bc383f9b56e3d
Luciano Ramos <lramosalves37@gmail.com>
2026-05-16 20:40:27 -0300

    update cleaning_candidatos.sql

--- project/election/sql/schema/prata/cleaning_candidatos.sql
+++ project/election/sql/schema/prata/cleaning_candidatos.sql
@@ -5,7 +5,8 @@ INSERT INTO prata_candidatos(
     partido,
     cargo_id,
     cidade,
-    idade
+    idade,
+    motivo_elegibilidade
 )
 SELECT 
 candidato_id,
@@ -13,12 +14,20 @@ CASE WHEN UPPER(TRIM(nome)) = ''
 OR UPPER(TRIM(nome)) IS NULL
 THEN 'Desconhecido' ELSE UPPER(TRIM(nome)) END AS nome,
 CASE WHEN UPPER(TRIM(nome)) = ''
-OR idade > 60  THEN 'Inelegivel' 
+OR idade > 60  
+OR cargo_id > 21
+THEN 'Inelegivel' 
 ELSE 'Elegivel' END AS status_elegibilidade,
 UPPER(TRIM(partido)) AS partido,
-TRIM(cargo) AS cargo,
+cargo_id,
 CASE WHEN UPPER(TRIM(cidade)) IS NULL THEN 'N/A' ELSE UPPER(TRIM(cidade)) END AS cidade,
-idade
+idade,
+CASE
+WHEN UPPER(TRIM(nome)) = '' OR nome IS NULL THEN 'Nome Invalido'
+WHEN idade > 60 THEN 'Idade acima de 60 anos'
+WHEN cargo_id > 21 THEN 'Cargo inexistente'
+ELSE 'Sem problemas' END
+AS motivo_elegibilidade
 FROM(
     select *,
     ROW_NUMBER() OVER(PARTITION BY candidato_id ORDER BY candidato_id DESC) AS flag_last
@@ -28,3 +37,6 @@ t WHERE flag_last = 1
 ORDER BY nome DESC;
 
 truncate table prata_candidatos
+
+set global local_infile = 1;
+SHOW VARIABLES LIKE 'local_infile';
