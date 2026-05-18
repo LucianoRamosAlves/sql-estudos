c8280349c8f488a4ce6906a67d58b4f94c0938db
Luciano Ramos <lramosalves37@gmail.com>
2026-05-18 13:31:24 -0300

    create \insert_cargos.sql

---
+++ project/election/sql/schema/ouro/insert_cargos.sql
@@ -0,0 +1,16 @@
+INSERT INTO ouro_cargos(
+    nome,
+    esfera,
+    vagas,
+    status_cargo
+)
+
+
+SELECT 
+nome_cargo,
+esfera,
+vagas,
+status_vagas
+FROM prata_cargos;
+
+select * from ouro_cargos;
